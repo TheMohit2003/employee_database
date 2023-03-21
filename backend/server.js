0b8ec1ac06e02081cc832e58eb80efa1f75a5db8
mohit <mohitpardeshi729@gmail.com>
2023-03-21 12:53:04 +0530

    added middleware to handle body data from request

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,12 @@ const app = express();
 require('dotenv').config();
 const db = require('./config/db');
 
+//database connection initiated
 db();
+// Middleware to parse JSON data
+app.use(express.json());
+// Middleware to parse URL-encoded data
+app.use(express.urlencoded({ extended: true }));
 
 app.use('/', require('./routes/employee'));
 
