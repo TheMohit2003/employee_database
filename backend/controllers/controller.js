52f5c34c93e62195c5243b67baa064d1cc4ab03d
mohit <mohitpardeshi729@gmail.com>
2023-03-21 12:56:27 +0530

    added api routes to controller file

--- backend/controllers/controller.js
+++ backend/controllers/controller.js
@@ -1,33 +1,52 @@
+const expressAsyncHandler = require('express-async-handler');
+const Data = require('../models/employeeData');
+
 //@description      all data of employees can be accessed
 //@route            GET
 //@access            private
 
-const getData = async (req, res) => {
-  res.send('this is the getData request');
-};
+const getData = expressAsyncHandler(async (req, res) => {
+  const data = await Data.find({});
+  res.status(200).json(data);
+});
 
 //@description      adding new employee to database
 //@route            POST
 //@access            private
 
-const setData = async (req, res) => {
-  res.send('this is the setData request');
-};
+const setData = expressAsyncHandler(async (req, res) => {
+  const { name, address, age, department, status } = req.body;
+  const employee = new Data({
+    name,
+    address,
+    age,
+    department,
+    status,
+  });
+  const employeeSaved = await employee.save();
+  res.status(200).json(employeeSaved);
+});
 
 //@description      data of chosen employee to be changed
 //@route            PUT
 //@access            private
 
-const putData = async (req, res) => {
-  res.send('this is the putData request');
-};
+const putData = expressAsyncHandler(async (req, res) => {
+  const id = req.params.id;
+  const updateEmployeeData = await Data.findByIdAndUpdate(id, req.body, {
+    new: true,
+  });
+  res.status(200).json(updateEmployeeData);
+});
 
 //@description      data of on specific employee is deleted
 //@route            DELETE
 //@access            private
 
-const deleteData = async (req, res) => {
-  res.send('this is the deleteData request');
-};
+const deleteData = expressAsyncHandler(async (req, res) => {
+  const id = req.params.id;
+  const deletedData = await Data.findByIdAndDelete(id);
+  res.status(200).json(deletedData);
+});
 
 module.exports = { getData, setData, putData, deleteData };
