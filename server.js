1c279d33a4f5cdb4d42e8c55d2a2f6d84728b4da
mohit <mohitpardeshi729@gmail.com>
2023-03-30 10:59:06 +0530

    changed the server intiating log statement

--- server.js
+++ server.js
@@ -12,9 +12,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use('/', require('./routes/employee'));
+// app.use('/auth', require('./routes/auth'));    //under progress
 
 app.use(errorHandler);
 
 app.listen(port, () => {
-  console.log('the server is running');
+  console.log(`server is running on port ${port}`);
 });
